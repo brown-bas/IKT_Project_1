2a1bc71d7c7f969b11d65ccc869e7d0cf6f5178a
brown-bas <Barnusfutureonline@gmail.com>
2023-09-26 01:18:29 +0200

    TailwindCSS konfigurálva

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
 <script>
   import Header from '$lib/components/Header.svelte';
+  import '../app.css';
 </script>
 
 <Header/>
 <slot/>
 
-<style>
+<style lang="postcss">
   @import url('https://fonts.googleapis.com/css2?family=Mukta:wght@400;600;800&display=swap');
 
   :global(body){
